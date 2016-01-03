c63a68a19c120cb8cdf6c204a8b737a8fffc9428
airshot16 <airshot16@gmail.com>
2016-01-03 19:09:20 +0900

    1

--- note/schema/data/om_team.sql
+++ note/schema/data/om_team.sql
@@ -1,14 +1,14 @@
 
-insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('00600FN0','team_finance@sbmkorea.com','chonj@sbmkorea.com','Y');
-insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('00600MF0','team_production@sbmkorea.com','lksdream@sbmkorea.com','Y');
-insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('00600MT0','team_mtech@sbmkorea.com','lapis76k@sbmkorea.com','Y');
-insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('00600PC0','team_procurement@sbmkorea.com','wch7519@sbmkorea.com','Y');
-insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('00600QC0','team_qc@sbmkorea.com','bungzo@sbmkorea.com','Y');
-insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('00600SL0','team_sales@sbmkorea.com','arthurkim@sbmkorea.com','Y');
-insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('00600ST0','team_strategy@sbmkorea.com','mireo1@sbmkorea.com','Y');
-insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('0060HQ00','team_heads@sbmkorea.com',null,'Y');
-insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('0060RHW0','team_hw@sbmkorea.com','mskim@sbmkorea.com','Y');
-insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('0060RMC0','team_mechanics@sbmkorea.com','cadwon@sbmkorea.com','Y');
-insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('0060RSW1','team_sw1@sbmkorea.com','mnsong@sbmkorea.com','Y');
-insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('0060RSW2','team_sw2@sbmkorea.com','joshin74@sbmkorea.com','Y');
-insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('0060RSW3','team_sw3@sbmkorea.com','jpark@sbmkorea.com','Y');
+insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('00600FN0','sales@trdoc.net','safe@trdoc.net','Y');
+insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('00600MF0','sales@trdoc.net','safe@trdoc.net','Y');
+insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('00600MT0','sales@trdoc.net','safe@trdoc.net','Y');
+insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('00600PC0','sales@trdoc.net','safe@trdoc.net','Y');
+insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('00600QC0','sales@trdoc.net','safe@trdoc.net','Y');
+insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('00600SL0','sales@trdoc.net','safe@trdoc.net','Y');
+insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('00600ST0','sales@trdoc.net','safe@trdoc.net','Y');
+insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('0060HQ00','sales@trdoc.net','safe@trdoc.net','Y');
+insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('0060RHW0','sales@trdoc.net','safe@trdoc.net','Y');
+insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('0060RMC0','sales@trdoc.net','safe@trdoc.net','Y');
+insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('0060RSW1','sales@trdoc.net','safe@trdoc.net','Y');
+insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('0060RSW2','sales@trdoc.net','safe@trdoc.net','Y');
+insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('0060RSW3','sales@trdoc.net','safe@trdoc.net','Y');
