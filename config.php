291b54f102ec80faf9f0d1e7ac01bf178deacafa
airshot16 <airshot16@gmail.com>
2016-06-16 02:33:29 +0900

    1

--- config.php
+++ config.php
@@ -11,6 +11,12 @@ define('LOCAL_SMTP_USER', 'kpbaek');
 define('LOCAL_SMTP_PASS', '1111');
 define('LOCAL_SMTP_PORT', '25');
 
+define('LOCAL_PUB_EMAIL', 'test@localhost');
+define('LOCAL_PUB2_EMAIL', 'test@localhost');
+define('LOCAL_SALES_EMAIL', 'test@localhost');
+define('LOCAL_DOMAIN', 'http://localhost:9090');
+
+define('LOCAL_TEST_EMAIL', 'test@localhost');
 define('SBM_LOCAL_EMAIL', 'test@localhost');
 
 define('SBM_SMTP_HOST', 'mx1.hostinger.kr');
@@ -22,4 +28,5 @@ define('SBM_PUB_EMAIL', 'trdoc@trdoc.net');
 define('SBM_PUB2_EMAIL', 'help@trdoc.net');
 define('SBM_SALES_EMAIL', 'order@trdoc.net');
 define('SBM_DOMAIN', 'http://www.trdoc.net');
+#define('SBM_DOMAIN', 'http://localhost:9090');
 
